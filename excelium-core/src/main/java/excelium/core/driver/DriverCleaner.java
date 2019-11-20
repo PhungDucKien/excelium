cf95bb03ec67e9004947298a90a43eb4fafe923b
Phung Duc Kien <kien.pd@altplus.com.vn>
2019-11-20 14:21:06 +0700

    Prints driver cleaner logs

--- excelium-core/src/main/java/excelium/core/driver/DriverCleaner.java
+++ excelium-core/src/main/java/excelium/core/driver/DriverCleaner.java
@@ -30,6 +30,8 @@ import excelium.model.test.config.MobileWebEnvironment;
 import excelium.model.test.config.PcEnvironment;
 import io.appium.java_client.AppiumDriver;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NotFoundException;
@@ -41,6 +43,8 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class DriverCleaner {
+
+    private static final Logger LOG = LogManager.getLogger();
 
     /**
      * Default implicit wait (in milliseconds) for web driver
@@ -113,6 +117,7 @@ public class DriverCleaner {
             }
             return true;
         } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
             return false;
         }
     }
