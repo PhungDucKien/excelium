a96bbf32347d0160830da291d858eb394c800363
Phung Duc Kien <kien.pd@altplus.com.vn>
2020-01-03 17:18:31 +0700

    migrate model

--- excelium-common/src/main/java/excelium/common/StringUtil.java
+++ excelium-common/src/main/java/excelium/common/StringUtil.java
@@ -31,64 +31,7 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Collection of string handling utilities
- *
- * @author PhungDucKien
- * @since 2018.04.11
- */
 public class StringUtil {
-
-    /**
-     * Get first sentence regex
-     */
-    private static final String GET_FIRST_SENTENCE_REGEX = "^(.*?[.!?])(?:\\s|$)(?!.*\\))";
-
-    /**
-     * Extract spreadsheet id from a spreadsheet url.
-     *
-     * @param url the spreadsheet url
-     * @return the spreadsheet id. Return the given string if it is not a spreadsheet url.
-     * @see <a href="https://developers.google.com/sheets/api/guides/concepts">Key Concepts in the Google Sheets API</a>
-     */
-    public static String extractSpreadsheetId(String url) {
-        String regex = "/d/([a-zA-Z0-9-_]+)";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(url);
-
-        if (matcher.find()) {
-            return matcher.group(1);
-        }
-        return url;
-    }
-
-    /**
-     * Converts byte size to human readable format.
-     * Eg: 1000 to "1 kB", 1000000 to "1 MB" etc.
-     *
-     * @param bytes byte size
-     * @param si    decimal format
-     * @return human readable byte size
-     * @see <a href="http://programming.guide/java/formatting-byte-size-to-human-readable-format.html">Java: Formatting byte size to human readable format</a>
-     */
-    public static String humanReadableByteCount(long bytes, boolean si) {
-        int unit = si ? 1000 : 1024;
-        if (bytes < unit) return bytes + " B";
-        int exp = (int) (Math.log(bytes) / Math.log(unit));
-        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
-        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
-    }
-
-    /**
-     * Adds spaces to camel case.
-     *
-     * @param input the input string in camel case
-     * @return the result string
-     */
-    public static String addSpaceToCamelCase(String input) {
-        return input.replaceAll("(\\P{Ll})(\\P{Ll}\\p{Ll})","$1 $2").replaceAll("(\\p{Ll})(\\P{Ll})","$1 $2");
-    }
 
     /**
      * Parse configuration string to properties
@@ -104,48 +47,5 @@ public class StringUtil {
             properties.load(reader);
         }
         return properties;
-    }
-
-    /**
-     * Returns the first sentence of the input string.
-     * The sentence ends with ends with "." or "!" or "?" and is followed by a white space character or the end of the line.
-     * E.g:
-     * 'This is first (e.g. 1st) sentence. And the second one.' returns 'This is first (e.g. 1st) sentence.'
-     * 'I need this domain.com! Another sentence. And another sentence.' returns 'I need this domain.com!'
-     *
-     * @param input the input string
-     * @return the first sentence
-     */
-    public static String getFirstSentence(String input) {
-        Pattern p = Pattern.compile(GET_FIRST_SENTENCE_REGEX);
-        Matcher m = p.matcher(input);
-        String firstSentence;
-        if (m.find()) {
-            firstSentence = m.group(1);
-        } else {
-            firstSentence = input;
-        }
-        return firstSentence;
-    }
-
-    /**
-     * Returns the name with the number of parameters. The name can be parameterized by declaring
-     * parameter names surrounded by square brackets.
-     *
-     * For instance, "Button [param1, param2]" takes param1 and param2 as its parameters.
-     *
-     * This method converts "Button [param1, param2]" into "Button [2]"
-     *
-     * @param name the name with parameters
-     * @return the name with the number of parameters
-     */
-    public static String getNameWithNumberOfParams(String name) {
-        Pattern p = Pattern.compile("\\b[^()]+\\[(.*)\\]$");
-        Matcher m = p.matcher(name);
-        if (m.find()) {
-            String params = m.group(1);
-            return name.substring(0, m.start(1)) + params.split(",").length + name.substring(m.end(1));
-        }
-        return name;
     }
 }
--- excelium-common/src/test/java/excelium/common/StringUtilTest.java
+++ excelium-common/src/test/java/excelium/common/StringUtilTest.java
@@ -30,63 +30,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.Properties;
 
-/**
- * Tests for {@link StringUtil}.
- *
- * @author PhungDucKien
- * @since 2018.04.11
- */
 public class StringUtilTest {
-
-    @Test
-    public void testExtractSpreadsheetId() {
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("https://docs.google.com/spreadsheets/d/1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU"));
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("https://docs.google.com/spreadsheets/d/1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU/"));
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("https://docs.google.com/spreadsheets/d/1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU/edit#gid=1160173419"));
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("https://docs.google.com/spreadsheets/u/0/d/1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU"));
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("https://docs.google.com/spreadsheets/u/0/d/1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU/"));
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("https://docs.google.com/spreadsheets/u/0/d/1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU/edit#gid=1160173419"));
-        Assert.assertEquals("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU", StringUtil.extractSpreadsheetId("1c87yUrvu-j-2Ve55NM8hYxYCkorcPm3W9VwHR_hDThU"));
-    }
-
-    @Test
-    public void testHumanReadableByteCount() {
-        Assert.assertEquals("0 B", StringUtil.humanReadableByteCount(0, true));
-        Assert.assertEquals("0 B", StringUtil.humanReadableByteCount(0, false));
-        Assert.assertEquals("27 B", StringUtil.humanReadableByteCount(27, true));
-        Assert.assertEquals("27 B", StringUtil.humanReadableByteCount(27, false));
-        Assert.assertEquals("999 B", StringUtil.humanReadableByteCount(999, true));
-        Assert.assertEquals("999 B", StringUtil.humanReadableByteCount(999, false));
-        Assert.assertEquals("1.0 kB", StringUtil.humanReadableByteCount(1000, true));
-        Assert.assertEquals("1000 B", StringUtil.humanReadableByteCount(1000, false));
-        Assert.assertEquals("1.0 kB", StringUtil.humanReadableByteCount(1023, true));
-        Assert.assertEquals("1023 B", StringUtil.humanReadableByteCount(1023, false));
-        Assert.assertEquals("1.0 kB", StringUtil.humanReadableByteCount(1024, true));
-        Assert.assertEquals("1.0 KiB", StringUtil.humanReadableByteCount(1024, false));
-        Assert.assertEquals("1.7 kB", StringUtil.humanReadableByteCount(1728, true));
-        Assert.assertEquals("1.7 KiB", StringUtil.humanReadableByteCount(1728, false));
-        Assert.assertEquals("110.6 kB", StringUtil.humanReadableByteCount(110592, true));
-        Assert.assertEquals("108.0 KiB", StringUtil.humanReadableByteCount(110592, false));
-        Assert.assertEquals("7.1 MB", StringUtil.humanReadableByteCount(7077888, true));
-        Assert.assertEquals("6.8 MiB", StringUtil.humanReadableByteCount(7077888, false));
-        Assert.assertEquals("453.0 MB", StringUtil.humanReadableByteCount(452984832, true));
-        Assert.assertEquals("432.0 MiB", StringUtil.humanReadableByteCount(452984832, false));
-        Assert.assertEquals("29.0 GB", StringUtil.humanReadableByteCount(28991029248L, true));
-        Assert.assertEquals("27.0 GiB", StringUtil.humanReadableByteCount(28991029248L, false));
-        Assert.assertEquals("1.9 TB", StringUtil.humanReadableByteCount(1855425871872L, true));
-        Assert.assertEquals("1.7 TiB", StringUtil.humanReadableByteCount(1855425871872L, false));
-        Assert.assertEquals("9.2 EB", StringUtil.humanReadableByteCount(Long.MAX_VALUE, true));
-        Assert.assertEquals("8.0 EiB", StringUtil.humanReadableByteCount(Long.MAX_VALUE, false));
-    }
-
-    @Test
-    public void testAddSpaceToCamelCase() {
-        Assert.assertEquals("First Name", StringUtil.addSpaceToCamelCase("FirstName"));
-        Assert.assertEquals("This Is My Caps Delimited String", StringUtil.addSpaceToCamelCase("ThisIsMyCapsDelimitedString"));
-        Assert.assertEquals("IBM Make Stuff And Sell It", StringUtil.addSpaceToCamelCase("IBMMakeStuffAndSellIt"));
-        Assert.assertEquals("big ID Problem", StringUtil.addSpaceToCamelCase("bigIDProblem"));
-        Assert.assertEquals("AWS File", StringUtil.addSpaceToCamelCase("AWSFile"));
-    }
 
     @Test
     public void testParseProperties() throws IOException {
@@ -95,15 +39,5 @@ public class StringUtilTest {
         Assert.assertEquals("d", properties.get("c"));
         Assert.assertEquals("", properties.get("e"));
         Assert.assertEquals("", properties.get("f"));
-    }
-
-    @Test
-    public void testGetFirstSentence() {
-        Assert.assertEquals("This is the first sentence", StringUtil.getFirstSentence("This is the first sentence"));
-        Assert.assertEquals("This is the first sentence.", StringUtil.getFirstSentence("This is the first sentence."));
-        Assert.assertEquals("This is the first sentence.This is the second sentence.", StringUtil.getFirstSentence("This is the first sentence.This is the second sentence."));
-        Assert.assertEquals("This is the first sentence.", StringUtil.getFirstSentence("This is the first sentence. This is the second sentence."));
-        Assert.assertEquals("This is first (e.g. 1st) sentence.", StringUtil.getFirstSentence("This is first (e.g. 1st) sentence. And the second one."));
-        Assert.assertEquals("I need this domain.com!", StringUtil.getFirstSentence("I need this domain.com! Another sentence. And another sentence."));
     }
 }
